93ad932927537d16146248e2a49f68a8a0dae896
3illBaby <thrillanon@outlook.com>
2023-10-30 22:12:04 +0100

    #4 <cleaned up the frontend>

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+      },
     },
   },
   plugins: [],
