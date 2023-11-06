56c5c75a61311ed8a2a0afed44680aade9da1e69
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-11-06 16:01:32 +0100

    feat: setup basic routes

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <>
+    <div>
+      <h1 className=" min-h-screen text-4xl text-black">In Development</h1>
+    </div>
+    </>
+  );
+};
+
+export default Profile;
