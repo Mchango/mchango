070f9c08275fc757b50ea70d4a5e670e48e83f23
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-11-17 14:00:18 +0100

    feat: create landing page

--- frontend/app/landing/page.tsx
+++ frontend/app/landing/page.tsx
@@ -1,6 +1,5 @@
-import Image from 'next/image'
-import React from 'react';
-import whitelogo from '../assets/whitelogo.svg'
+
+import Navbar from '../components/Navbar';
 
 const Landing = () => {
   return (
@@ -8,20 +7,14 @@ const Landing = () => {
    <div className='flex justify-center'>
     <div className='w-full'>
       <div className='h-screen w-full bg-[url("https://res.cloudinary.com/dmye53wps/image/upload/v1699652923/mchango_m0fuc7.svg")] bg-cover bg-center '>
-        <div className='flex w-[90%] justify-between mx-auto  '>
-          <div className='mt-[26px]'>
-            <Image src={whitelogo} alt='logo' className='' />
-          </div>
-          <div className=''>
-            <div className='text-[#fff] tex-[18px] font-medium tracking-[0.36px] '>Sign Up</div>
-          </div>
-
-        </div>
+        <Navbar />
+        
       </div>
 
     </div>
    </div>
     </>
+
   )
 }
 
