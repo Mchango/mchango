24c00ba675e582dc9446d433de70523ead8b3cad
3illBaby <thrillanon@outlook.com>
2023-11-17 12:36:38 +0100

    Added more functions to helper contract

--- contract/contracts/Helper.sol
+++ contract/contracts/Helper.sol
@@ -12,11 +12,46 @@ library Helper {
         return result;
     }
 
-    function removeIndex(uint _id, address[] storage _array) internal {
+    function calculateIndexToRemove(
+        address _addressToRemove,
+        address[] storage _array
+    ) internal view returns (uint indexToRemove) {
+        for (uint i = 0; i < _array.length; i++) {
+            if (_array[i] == _addressToRemove) {
+                indexToRemove = i;
+            }
+        }
+    }
+
+    function shiftAndRemoveIndex(uint _id, address[] storage _array) internal {
         for (uint i = _id; i < _array.length - 1; i++) {
             _array[i] = _array[i + 1];
         }
 
         _array.pop();
+    }
+
+    function removeAddress(
+        address _addressToRemove,
+        address[] storage _array
+    ) internal {
+        for (uint i = 0; i < _array.length; i++) {
+            if (_array[i] == _addressToRemove) {
+                _array[i] = _array[_array.length - 1];
+            }
+            _array.pop();
+        }
+    }
+
+    function shiftAndRemoveAddress(
+        address _addressToRemove,
+        address[] storage _array
+    ) internal {
+        for (uint i = 0; i < _array.length; i++) {
+            if (_array[i] == _addressToRemove) {
+                _array[i] = _array[i + 1];
+            }
+            _array.pop();
+        }
     }
 }
