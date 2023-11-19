a5088011dfb4d173b1902d86a6a3017eb76c706d
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-11-19 17:44:55 +0100

    feat: import in landing page

--- frontend/app/landing/page.tsx
+++ frontend/app/landing/page.tsx
@@ -1,14 +1,17 @@
 
+import GatewayLanding from '../components/GatewayLanding';
 import Navbar from '../components/Navbar';
 
 const Landing = () => {
   return (
     <>
    <div className='flex justify-center'>
-    <div className='w-full'>
+    <div className='w-full '>
       <div className='h-screen w-full bg-[url("https://res.cloudinary.com/dmye53wps/image/upload/v1699652923/mchango_m0fuc7.svg")] bg-cover bg-center '>
+        <div className='lg:max-w-7xl w-full mx-auto'>
         <Navbar />
-        
+        <GatewayLanding />
+        </div>
       </div>
 
     </div>
