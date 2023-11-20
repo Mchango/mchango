db866af2146d819220d2eeec08aa40c6f229350e
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-11-20 18:23:42 +0100

    fix: update nav gradients

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
           </div>
           <div className='flex'>
             <div className='text-[#fff] text-[20px] font-semibold tracking-[0.36px] text-center my-auto mr-[29px] cursor-pointer hover:text-[teal] '>Sign In</div>
-            <button className='text-[16px] tracking-[0.8px] text-[#fff] py-[18px] px-[28px] rounded-[15px] bg-gradient-to-r from-teal-800 to-purple-700 hover:to-teal-700 gap-[10px] w-[210px] h-[58px] '>Get Started</button>
+            <button className='text-[16px] tracking-[0.8px] text-[#fff] py-[18px] px-[28px] rounded-[15px] bg-[#008080] hover:bg-[#096b6b] gap-[10px] w-[210px] h-[58px] '>Get Started</button>
           </div>
         </div>
         <Image src={line} alt='line' className='mx-auto w-[90%]' />
