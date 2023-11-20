a9597f1980125a6c251637cb6c885646d3048dd0
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-11-20 18:24:57 +0100

    svgs

--- frontend/app/landing/page.tsx
+++ frontend/app/landing/page.tsx
@@ -1,16 +1,18 @@
 
 import GatewayLanding from '../components/GatewayLanding';
 import Navbar from '../components/Navbar';
+import SmartCrypto from '../components/SmartCrypto';
 
 const Landing = () => {
   return (
     <>
    <div className='flex justify-center'>
     <div className='w-full '>
-      <div className='h-screen w-full bg-[url("https://res.cloudinary.com/dmye53wps/image/upload/v1699652923/mchango_m0fuc7.svg")] bg-cover bg-center '>
+      <div className='h-full w-full bg-[url("https://res.cloudinary.com/dmye53wps/image/upload/v1699652923/mchango_m0fuc7.svg")] bg-cover bg-center '>
         <div className='lg:max-w-7xl w-full mx-auto'>
         <Navbar />
         <GatewayLanding />
+        <SmartCrypto />
         </div>
       </div>
 
