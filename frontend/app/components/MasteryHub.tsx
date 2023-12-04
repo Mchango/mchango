e32e31d02595714845bcf14c27b3f879f3210935
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-04 01:11:31 +0100

    feat: mastery hub background image

--- frontend/app/components/MasteryHub.tsx
+++ frontend/app/components/MasteryHub.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import Image from 'next/image'
+import React from 'react';
+import mastery from '../assets/mastery.png'
 
 const MasteryHub = () => {
   return (
     <>
-    <div className='flex justify-center'>
+      <div className='flex justify-center mb-[150px]'>
+        <div className='w-full'>
+          <div className='relative'>
+            <div className='absolute w-auto'>
+              <div className='text-[40px] font-extrabold leading-[48px] -tracking-[1.2px] '>Mchango_ Mastery Hub</div>
+              <div className='text-[16px] font-medium -tracking-[0.32p] leading-[32px] my-[22px]  '>Embark on a journey to financial empowerment with Mchango_. Discover a step-by-step guide to collaborative savings, real-time insights, and community-driven prosperity.</div>
+              
+            </div>
+            <Image src={mastery} alt='icon' className='relative' />
 
-    </div>
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
