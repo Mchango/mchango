228ad2381b983c900d66bfc4ce2caaba0a6b7425
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-06 08:05:15 +0100

    feat: complete mastery hub component

--- frontend/app/components/MasteryHub.tsx
+++ frontend/app/components/MasteryHub.tsx
@@ -9,8 +9,9 @@ const MasteryHub = () => {
         <div className='w-full'>
           <div className='relative'>
             <div className='absolute w-auto'>
-              <div className='text-[40px] font-extrabold leading-[48px] -tracking-[1.2px] '>Mchango_ Mastery Hub</div>
-              <div className='text-[16px] font-medium -tracking-[0.32p] leading-[32px] my-[22px]  '>Embark on a journey to financial empowerment with Mchango_. Discover a step-by-step guide to collaborative savings, real-time insights, and community-driven prosperity.</div>
+              <div className='text-[40px] font-extrabold leading-[48px] -tracking-[1.2px] text-[#FFFFFF] '>Mchango_ Mastery Hub</div>
+              <div className='text-[16px] font-medium -tracking-[0.32p] leading-[32px] my-[22px] text-[#FFFFFF] w-[367px]  '>Embark on a journey to financial empowerment with Mchango_. Discover a step-by-step guide to collaborative savings, real-time insights, and community-driven prosperity.</div>
+              <button className='border-[1px] border-[#008080] w-[151px] h-[49px] rounded-[15px] text-[#FFFFFF] text-[16px] tracking-[0.8px] '>Get Started</button>
               
             </div>
             <Image src={mastery} alt='icon' className='relative' />
