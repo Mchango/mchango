e779ee8029a8e408f25ac2d8e5715a8634e882b5
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-08 18:26:07 +0100

    fix: input

--- frontend/app/components/SignupForm.tsx
+++ frontend/app/components/SignupForm.tsx
@@ -24,10 +24,10 @@ const SignupForm = () => {
             </div>
             <div className='flex flex-col mb-[39px]'>
                 <label className='text-[16px] leading-[16px] tracking-[0.4px] text-[#FFFFFF] ml-[10px] '> Phone Number</label>
-                <input className='mt-[17px] opacity-[0.8] rounded-[20px] text-[20px] font-semibold tracking-[0.1px] leading-[24px] text-[#000000] pl-[34px] py-[23px] w-[507px] outline outline-none cursor-pointer hover:bg-[#008080] ' placeholder='+407'/>
+                <input className='mt-[17px] opacity-[0.8] rounded-[20px] text-[20px] font-semibold tracking-[0.1px] leading-[24px] text-[#000000] pl-[34px] py-[23px] w-[507px] outline outline-none ' placeholder='+407'/>
             </div>
             <div className='flex justify-center mb-[100px]'>
-            <button className='signupbtn text-[16px] tracking-[0.8px] text-[#fff] w-[165px] h-[58px] mx-auto '>
+            <button className='signupbtn text-[16px] tracking-[0.8px] text-[#fff] w-[165px] h-[58px] mx-auto cursor-pointer '>
             Sign Up
             </button>
             </div>
