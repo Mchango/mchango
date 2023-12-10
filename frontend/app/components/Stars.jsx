e0f6b1087f01c82c71fc32fdd42233bfe4d738b2
3illBaby <thrillanon@outlook.com>
2023-12-10 17:53:41 +0100

    Added Starry Animation

---
+++ frontend/app/components/Stars.jsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useRef, Suspense } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Points, PointMaterial, Preload } from '@react-three/drei';
+import * as random from 'maath/random/dist/maath-random.esm';
+
+const Stars = (props) => {
+  const ref = useRef();
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.1 });
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
+  return (
+    // eslint-disable-next-line react/no-unknown-property
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={4} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="#ffff"
+          size={0.005}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
+
+const StarCanvas = () => {
+  return (
+    <div className=" h-screen w-full ">
+      <Canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </div>
+  );
+};
+
+export default StarCanvas;
