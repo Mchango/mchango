a5f9ee4131b1483e1f476a25a8aa857e4aca33d5
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-10 16:35:22 +0100

    feat: complete sideNav component

--- frontend/app/components/SideNav.tsx
+++ frontend/app/components/SideNav.tsx
@@ -5,13 +5,14 @@ import home from '../assets/home.svg';
 import chat from '../assets/chat.svg';
 import group from '../assets/group.svg';
 import upgrade from '../assets/upgrade.svg';
-import profile from '../assets/profile.svg'
+import profile from '../assets/profile.svg';
+import rocket from '../assets/rocket.svg'
 
 const SideNav = () => {
     return (
         <>
             <div className='flex justify-center bg-[#011717]'>
-                <div className=' w-[239px] h-[900px] px-[24px] pt-[24px] '>
+                <div className=' w-[239px] pb-[81px] px-[24px] pt-[24px] '>
                     <div className='flex mb-[50px] '>
                         <Image src={mchlogo} alt='mchlogo' className='w-[38px] h-[39px] ' />
                         <div className='text-[25px] font-bold text-[#fff] ml-[9px] ' >Mchango_</div>
@@ -38,8 +39,14 @@ const SideNav = () => {
                             <div className='text-[20px] leading-[28px] text-[#EFEFEF] '>Profile</div>
                         </div>
                     </div>
-                    <div className='relative'>
-                        
+                    <div className='relative mt-[46px] '>
+                        <Image src={rocket} alt='rocket' className='-mb-[130px] mx-auto' />
+                        <div className='rocket pt-[130px] '>
+                            <div className='text-[12px] font-semibold text-[#fff] text-center w-[90%] mx-auto mt-[3px] mb-[8.8px] '>Want to upgrade to pro to get more exciting features?</div>
+                            <div className='flex justify-center pb-[26.8px] '>
+                            <button className='text-[12px] font-medium text-[#fff] w-[119px] h-[34px] rounded-[20.8px] bg-[#000000] mx-auto hover:bg-[#141313] '>Upgrade now</button>
+                            </div> 
+                        </div>
 
                     </div>
 
