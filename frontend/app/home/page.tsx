2614582aa746655f38e8a5a1c1d4df2e59b5eae7
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-10 15:50:27 +0100

    feat: create home page

---
+++ frontend/app/home/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import SideNav from '../components/SideNav'
+
+const Home = () => {
+    return (
+        <>
+            <div className='bg-[#021D1D] flex justify-center '>
+                <div className='w-full lg:max-w-[1400px]'>
+                    <div className='flex'>
+                        <SideNav />
+                        <div className='w-full'>kmcksmsecm
+                        <div>vjhhhjb</div>
+                        <div>
+                    jnbcejfejfnjefn
+                    <div>jndnjedjwndwjn</div>
+                    <div>jbdjwdj</div>
+                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
