f2478e461dd2619192012df11a65069a02333821
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-13 18:49:11 +0100

    feat: create wallet seach input

--- frontend/app/components/WalletNav.tsx
+++ frontend/app/components/WalletNav.tsx
@@ -21,7 +21,18 @@ const WalletNav = () => {
                             </li>
                         </ul>
                     </div>
-
+                    <form className="">
+                        <div className="relative">
+                            <div className="absolute inset-y-0 left-0 flex items-center pl-[20px] pointer-events-none">
+                                <svg className="w-[13px] text-gray-500 dark:text-gray-400 mt-[17px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                </svg>
+                            </div>
+                            <input
+                                className="inp bg-[#021d1d] border border-[#fff] text-white text-[12px] pl-[40px] w-[249px] h-[35px] mt-[20px] rounded-[30px] outline outline-none "
+                            />
+                        </div>
+                    </form>
                 </div>
 
             </div>
