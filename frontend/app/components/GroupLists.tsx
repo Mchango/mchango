04355247bf4e5ee62a00be49cfc2b333b6bfe078
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-30 21:26:10 +0100

    feat: complete grouplist component

--- frontend/app/components/GroupLists.tsx
+++ frontend/app/components/GroupLists.tsx
@@ -26,8 +26,8 @@ const GroupLists = () => {
             <div key={index} className='flex justify-center '>
               <div className='w-full mt-[34px]'>
                 <div className='ml-[64px]'>
-                  <div className='grid grid-cols-2 space-x-[35px] '>
-                    <div className='grpcard1 pl-[19px] pt-[12px] pr-[16px] '>
+                  <div className='grid grid-cols-2 '>
+                    <div className='grpcard1 pl-[19px] pt-[12px] pr-[16px] mb-[55px] '>
                     <div className='flex justify-between mb-[16px] '>
                       <Image src={adv} alt='adv' className='' />
                       <Image src={dot} alt='dot' className='' />
@@ -56,11 +56,11 @@ const GroupLists = () => {
                         <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
                       </div>
                     </div>
-                    <div className='mx-auto w-full flex justify-center mt-[14px] mb-[25px] '>
+                    <div className='mx-auto w-full flex justify-center mt-[14px] mb-[34px] '>
                       <button className='grpbtnn text-[20px] mx-auto font-bold leading-[24px] tracking-[0.1px] text-[#00000] w-[343px] h-[52px] '>View Group</button>
                     </div>
                     </div>
-                    <div className='grpcard1 pl-[19px] pt-[12px] pr-[16px] '>
+                    <div className='grpcard1 pl-[19px] pt-[12px] pr-[16px] mb-[55px] ml-[35px] '>
                     <div className='flex justify-between mb-[16px] '>
                       <Image src={adv} alt='adv' className='' />
                       <Image src={dot} alt='dot' className='' />
@@ -89,11 +89,11 @@ const GroupLists = () => {
                         <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
                       </div>
                     </div>
-                    <div className='mx-auto w-full flex justify-center mt-[14px] mb-[25px] '>
+                    <div className='mx-auto w-full flex justify-center mt-[14px] mb-[34px] '>
                       <button className='grpbtnn text-[20px] mx-auto font-bold leading-[24px] tracking-[0.1px] text-[#00000] w-[343px] h-[52px] '>View Group</button>
                     </div>
                     </div>
-                    <div className='grpcard1 pl-[19px] pt-[12px] pr-[16px] '>
+                    <div className='grpcard1 pl-[19px] pt-[12px] pr-[16px] mb-[55px] space-x-[0px] '>
                     <div className='flex justify-between mb-[16px] '>
                       <Image src={adv} alt='adv' className='' />
                       <Image src={dot} alt='dot' className='' />
@@ -122,7 +122,172 @@ const GroupLists = () => {
                         <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
                       </div>
                     </div>
-                    <div className='mx-auto w-full flex justify-center mt-[14px] mb-[25px] '>
+                    <div className='mx-auto w-full flex justify-center mt-[14px] mb-[34px] '>
+                      <button className='grpbtnn text-[20px] mx-auto font-bold leading-[24px] tracking-[0.1px] text-[#00000] w-[343px] h-[52px] '>View Group</button>
+                    </div>
+                    </div>
+                    <div className='grpcard1 pl-[19px] pt-[12px] pr-[16px] mb-[55px] ml-[35px] '>
+                    <div className='flex justify-between mb-[16px] '>
+                      <Image src={adv} alt='adv' className='' />
+                      <Image src={dot} alt='dot' className='' />
+                    </div>
+                    <div className='text-[26px] font-extrabold leading-[31.2px] text-[#fff] mb-[6px] '>{token.nameofgrp}</div>
+                    <div className='text-[12px] font-semibold leading-[13.2px] mb-[12px] text-[#fff] '>{token.grpdate}</div>
+                    <div className='grid grid-cols-4'>
+                      <div className='w-[91px] '>
+                        <Image src={locate} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Group Admin Address</div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                      <div className='w-[91px] '>
+                        <Image src={doll} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Collateral Amount</div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                      <div className='w-[107px] '>
+                        <Image src={people} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Total <p>No of Members</p></div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpno}</div>
+                      </div>
+                      <div className='w-[91px] '>
+                        <Image src={ticky} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Group <p>State</p></div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                    </div>
+                    <div className='mx-auto w-full flex justify-center mt-[14px] mb-[34px] '>
+                      <button className='grpbtnn text-[20px] mx-auto font-bold leading-[24px] tracking-[0.1px] text-[#00000] w-[343px] h-[52px] '>View Group</button>
+                    </div>
+                    </div>
+                    <div className='grpcard1 pl-[19px] pt-[12px] pr-[16px] mb-[55px] '>
+                    <div className='flex justify-between mb-[16px] '>
+                      <Image src={adv} alt='adv' className='' />
+                      <Image src={dot} alt='dot' className='' />
+                    </div>
+                    <div className='text-[26px] font-extrabold leading-[31.2px] text-[#fff] mb-[6px] '>{token.nameofgrp}</div>
+                    <div className='text-[12px] font-semibold leading-[13.2px] mb-[12px] text-[#fff] '>{token.grpdate}</div>
+                    <div className='grid grid-cols-4'>
+                      <div className='w-[91px] '>
+                        <Image src={locate} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Group Admin Address</div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                      <div className='w-[91px] '>
+                        <Image src={doll} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Collateral Amount</div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                      <div className='w-[107px] '>
+                        <Image src={people} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Total <p>No of Members</p></div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpno}</div>
+                      </div>
+                      <div className='w-[91px] '>
+                        <Image src={ticky} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Group <p>State</p></div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                    </div>
+                    <div className='mx-auto w-full flex justify-center mt-[14px] mb-[34px] '>
+                      <button className='grpbtnn text-[20px] mx-auto font-bold leading-[24px] tracking-[0.1px] text-[#00000] w-[343px] h-[52px] '>View Group</button>
+                    </div>
+                    </div>
+                    <div className='grpcard1 pl-[19px] pt-[12px] pr-[16px] mb-[55px] ml-[35px] '>
+                    <div className='flex justify-between mb-[16px] '>
+                      <Image src={adv} alt='adv' className='' />
+                      <Image src={dot} alt='dot' className='' />
+                    </div>
+                    <div className='text-[26px] font-extrabold leading-[31.2px] text-[#fff] mb-[6px] '>{token.nameofgrp}</div>
+                    <div className='text-[12px] font-semibold leading-[13.2px] mb-[12px] text-[#fff] '>{token.grpdate}</div>
+                    <div className='grid grid-cols-4'>
+                      <div className='w-[91px] '>
+                        <Image src={locate} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Group Admin Address</div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                      <div className='w-[91px] '>
+                        <Image src={doll} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Collateral Amount</div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                      <div className='w-[107px] '>
+                        <Image src={people} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Total <p>No of Members</p></div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpno}</div>
+                      </div>
+                      <div className='w-[91px] '>
+                        <Image src={ticky} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Group <p>State</p></div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                    </div>
+                    <div className='mx-auto w-full flex justify-center mt-[14px] mb-[34px] '>
+                      <button className='grpbtnn text-[20px] mx-auto font-bold leading-[24px] tracking-[0.1px] text-[#00000] w-[343px] h-[52px] '>View Group</button>
+                    </div>
+                    </div>
+                    <div className='grpcard1 pl-[19px] pt-[12px] pr-[16px] mb-[55px] '>
+                    <div className='flex justify-between mb-[16px] '>
+                      <Image src={adv} alt='adv' className='' />
+                      <Image src={dot} alt='dot' className='' />
+                    </div>
+                    <div className='text-[26px] font-extrabold leading-[31.2px] text-[#fff] mb-[6px] '>{token.nameofgrp}</div>
+                    <div className='text-[12px] font-semibold leading-[13.2px] mb-[12px] text-[#fff] '>{token.grpdate}</div>
+                    <div className='grid grid-cols-4'>
+                      <div className='w-[91px] '>
+                        <Image src={locate} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Group Admin Address</div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                      <div className='w-[91px] '>
+                        <Image src={doll} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Collateral Amount</div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                      <div className='w-[107px] '>
+                        <Image src={people} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Total <p>No of Members</p></div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpno}</div>
+                      </div>
+                      <div className='w-[91px] '>
+                        <Image src={ticky} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Group <p>State</p></div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                    </div>
+                    <div className='mx-auto w-full flex justify-center mt-[14px] mb-[34px] '>
+                      <button className='grpbtnn text-[20px] mx-auto font-bold leading-[24px] tracking-[0.1px] text-[#00000] w-[343px] h-[52px] '>View Group</button>
+                    </div>
+                    </div>
+                    <div className='grpcard1 pl-[19px] pt-[12px] pr-[16px] mb-[55px] ml-[35px] '>
+                    <div className='flex justify-between mb-[16px] '>
+                      <Image src={adv} alt='adv' className='' />
+                      <Image src={dot} alt='dot' className='' />
+                    </div>
+                    <div className='text-[26px] font-extrabold leading-[31.2px] text-[#fff] mb-[6px] '>{token.nameofgrp}</div>
+                    <div className='text-[12px] font-semibold leading-[13.2px] mb-[12px] text-[#fff] '>{token.grpdate}</div>
+                    <div className='grid grid-cols-4'>
+                      <div className='w-[91px] '>
+                        <Image src={locate} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Group Admin Address</div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                      <div className='w-[91px] '>
+                        <Image src={doll} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Collateral Amount</div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                      <div className='w-[107px] '>
+                        <Image src={people} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Total <p>No of Members</p></div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpno}</div>
+                      </div>
+                      <div className='w-[91px] '>
+                        <Image src={ticky} alt='png' className='' />
+                        <div className='text-[14px] font-bold text-[#fff] mt-[5px] mb-[8px] '>Group <p>State</p></div>
+                        <div className='text-[#00FFFF] text-[12px] font-semibold '>{token.grpether}</div>
+                      </div>
+                    </div>
+                    <div className='mx-auto w-full flex justify-center mt-[14px] mb-[34px] '>
                       <button className='grpbtnn text-[20px] mx-auto font-bold leading-[24px] tracking-[0.1px] text-[#00000] w-[343px] h-[52px] '>View Group</button>
                     </div>
                     </div>
