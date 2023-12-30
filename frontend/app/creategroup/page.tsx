56228e310cba241dfa15c7d3c25f1dd90877eba1
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-31 00:23:07 +0100

    feat: create create group page                       and import sideNav

---
+++ frontend/app/creategroup/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import SideNav from '../components/SideNav'
+import CreateGroup from '../components/CreateGroup'
+
+const CreateGrp = () => {
+  return (
+    <>
+    <div  className='bg-[#021D1D] flex justify-center '>
+        <div className='w-full lg:max-w-[1400px]'>
+            <div className='flex'>
+                <SideNav />
+                <CreateGroup />
+            </div>
+
+        </div>
+
+    </div>
+    </>
+
+    )
+}
+
+export default CreateGrp
