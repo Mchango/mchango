5186a42a115036622a1b71b9020a89315a976a06
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-01-07 19:43:04 +0100

    feat: create chats page

---
+++ frontend/app/chats/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import SideNav from '../components/SideNav'
+
+const Chats = () => {
+  return (
+   <>
+   <div className='bg-[#021D1D] flex justify-center '>
+    <div className='w-full lg:max-w-[1400px]'>
+        <div className='flex'>
+            <SideNav />
+
+        </div>
+    </div>
+
+   </div>
+   </>
+  )
+}
+
+export default Chats
