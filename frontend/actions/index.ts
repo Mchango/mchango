145a991995721d8608e234bc7ed03a9b57cdefee
3illBaby <thrillanon@outlook.com>
2024-01-24 13:29:28 +0100

    actions: completed create group function

--- frontend/actions/index.ts
+++ frontend/actions/index.ts
@@ -1,6 +1,15 @@
 import { connectDB } from '@/database/mongoose'
-import { createMember } from '@/database'
-import { createNewMember } from '@/contract-actions'
+import {
+  createMember,
+  createGroup,
+  handleGetNumberOfGroupsCreated,
+  getIsPremiumSubscriber,
+} from '@/database'
+import {
+  createNewGroup,
+  createNewMember,
+  getProviderAndSigner,
+} from '@/contract-actions'
 
 class MetaMaskError extends Error {}
 
@@ -30,4 +39,50 @@ const createUser = async (name: string) => {
   }
 }
 
-export { createUser }
+const createUserGroup = async (
+  collateralValue: string,
+  name: string,
+  description: string,
+) => {
+  try {
+    const { signer } = await getProviderAndSigner()
+    const walletAddress = await signer.getAddress()
+    if (!walletAddress || typeof walletAddress !== 'string')
+      throw new Error('An error occurred while getting signer')
+
+    const isPremiumSubscriber = await getIsPremiumSubscriber(walletAddress)
+    if (!isPremiumSubscriber) {
+      const numberOfGroupsCreated = await handleGetNumberOfGroupsCreated(
+        walletAddress,
+      )
+
+      if (numberOfGroupsCreated >= 2) {
+        throw new Error(
+          'You have reached your limit, upgrade to premium to create more groups',
+        )
+      }
+
+      const [formatted, numberFormat, signerAddress] = await createNewGroup(
+        collateralValue,
+      )
+      if (!formatted || !numberFormat || !signerAddress)
+        throw new Error('is metamask connected?')
+
+      await createGroup({
+        id: formatted as number,
+        name: name as string,
+        description: description as string,
+        collateral: numberFormat as number,
+
+        admin: signerAddress as string,
+      })
+
+      return console.log(`${name} was successfully created`)
+    }
+  } catch (error) {
+    console.error('An error occurred while creating a new group', error)
+    throw new Error('An error occurred while creating a new group')
+  }
+}
+
+export { createUser, createUserGroup }
