4427b67ee6077ee1b3b9a261e5c5af5e6bb7e13d
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-02-02 13:31:54 +0100

    feat: complete delware chat messages

--- frontend/app/components/DelwareChat.tsx
+++ frontend/app/components/DelwareChat.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import tokensData from '../data.json';
 import Image from 'next/image';
-import adex from '../assets/adex.png'
+import adex from '../assets/adex.png';
+import peter from '../assets/peter.png';
+import deset from '../assets/deset.png'
 
 interface Token {
     delwaretime: string;
@@ -14,6 +16,13 @@ interface Token {
     delmemb3name: string;
     delmsgtime3: string;
     delchat3: string;
+    delmemb4name: string;
+    delmsgtime4: string;
+    delchat4: string;
+    delmemb5name: string;
+    delmsgtime5: string;
+    delchat5: string;
+    
 
 }
 
@@ -33,6 +42,56 @@ const DelwareChat = () => {
                                 <div className='text-[#717171] text-[11px] leading-[16px] my-auto ml-[5px] '>{token.delmsgtime1}</div>
                             </div>
                             <div className='text-[15px] leading-[24px] font-light lg:w-[564px] '>{token.delchat1}</div>
+                        </div>
+                    </div>
+                    <div className='flex my-[41px] '>
+                        <Image src={peter} alt='profile-img' className='my-auto ' />
+                        <div className='ml-[12px] '>
+                            <div className='flex'>
+                                <div className='text-[16px] leading-[22px] font-extrabold '>{token.delmemb2name}</div>
+                                <div className='text-[#717171] text-[11px] leading-[16px] my-auto ml-[5px] '>{token.delmsgtime2}</div>
+                            </div>
+                            <div className='text-[15px] leading-[24px] font-light lg:w-[564px] '>{token.delchat2}</div>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <Image src={deset} alt='profile-img' className='my-auto ' />
+                        <div className='ml-[12px] '>
+                            <div className='flex'>
+                                <div className='text-[16px] leading-[22px] font-extrabold '>{token.delmemb3name}</div>
+                                <div className='text-[#717171] text-[11px] leading-[16px] my-auto ml-[5px] '>{token.delmsgtime3}</div>
+                            </div>
+                            <div className='text-[15px] leading-[24px] font-light lg:w-[564px] '>{token.delchat3}</div>
+                        </div>
+                    </div>
+                    <div className='flex my-[41px] '>
+                        <Image src={peter} alt='profile-img' className='my-auto ' />
+                        <div className='ml-[12px] '>
+                            <div className='flex'>
+                                <div className='text-[16px] leading-[22px] font-extrabold '>{token.delmemb4name}</div>
+                                <div className='text-[#717171] text-[11px] leading-[16px] my-auto ml-[5px] '>{token.delmsgtime4}</div>
+                            </div>
+                            <div className='text-[15px] leading-[24px] font-light lg:w-[564px] '>{token.delchat4}</div>
+                        </div>
+                    </div>
+                    <div className='flex'>
+                        <Image src={deset} alt='profile-img' className='my-auto ' />
+                        <div className='ml-[12px] '>
+                            <div className='flex'>
+                                <div className='text-[16px] leading-[22px] font-extrabold '>{token.delmemb5name}</div>
+                                <div className='text-[#717171] text-[11px] leading-[16px] my-auto ml-[5px] '>{token.delmsgtime5}</div>
+                            </div>
+                            <div className='text-[15px] leading-[24px] font-light lg:w-[564px] '>{token.delchat5}</div>
+                        </div>
+                    </div>
+                    <div className='flex my-[41px] '>
+                        <Image src={peter} alt='profile-img' className='my-auto ' />
+                        <div className='ml-[12px] '>
+                            <div className='flex'>
+                                <div className='text-[16px] leading-[22px] font-extrabold '>{token.delmemb2name}</div>
+                                <div className='text-[#717171] text-[11px] leading-[16px] my-auto ml-[5px] '>{token.delmsgtime2}</div>
+                            </div>
+                            <div className='text-[15px] leading-[24px] font-light lg:w-[564px] '>{token.delchat2}</div>
                         </div>
                     </div>
                 </div>
