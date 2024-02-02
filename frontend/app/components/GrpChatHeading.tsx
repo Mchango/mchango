16f4267f3ad4be308acbe271dec939f1d3abe1d6
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-02-02 12:32:47 +0100

    feat: complete group chat heading

--- frontend/app/components/GrpChatHeading.tsx
+++ frontend/app/components/GrpChatHeading.tsx
@@ -15,7 +15,7 @@ const GrpChatHeading = () => {
   return (
     <>
     {tokens.map((token, index) => (
-    <div className='flex justify-center w-[688px] w-full ' key={index}>
+    <div className='flex justify-center lg:w-[688px] ' key={index}>
         <div className='w-full  '>
         <div className='flex justify-between text-[#fff] w-full '>
             <div className='text-[20px] leading-[24px] font-extrabold '>
@@ -27,10 +27,10 @@ const GrpChatHeading = () => {
                 <Image src={add} alt='add' className='mx-auto' /> 
                 <div>Add Member</div>
               </div>
-
+              <button className='ml-[16px] my-auto lg:w-[111.6px] h-[36px] bg-[#008080] rounded-[30px] text-[12px] leading-[16px] font-bold '>Group details</button>
             </div>
         </div>
-
+        <hr className='mt-[8px] mb-[23px] '/>
         </div>
 
     </div>
