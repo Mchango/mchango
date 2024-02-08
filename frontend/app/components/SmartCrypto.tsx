e40a0e3806799f2752ab2b6f1c9e310986251358
3illBaby <thrillanon@outlook.com>
2024-02-09 00:17:40 +0100

    chore: bulk edit, heavy refactor

--- frontend/app/components/SmartCrypto.tsx
+++ frontend/app/components/SmartCrypto.tsx
@@ -1,43 +1,93 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 import invest from '../assets/invest.svg';
 import collab from '../assets/collab.svg';
-import auto from '../assets/auto.svg'
+import auto from '../assets/auto.svg';
+import { BackgroundGradient } from './BackgroundGradient';
 
 const SmartCrypto = () => {
   return (
     <>
-      <div className='flex justify-center mt-[70px] lg:mt-[76px] mb-[150px] '>
-        <div className='w-full'>
-          <div className='lg:text-[50px] text-[20px] leading-[22px] font-extrabold lg:leading-[55px] text-[#D9D9DB] lg:w-[734px] mx-auto text-center lg:leading-normal '>Smart Crypto Investing <br />and Community Empowerment</div>
-          <div className=''>
-            <div className='grid lg:grid-cols-3 justify-between'>
-              <div className='smartbg py-[32px] px-[10px]'>
-                <Image src={invest} alt='icon' className='w-[300px] h-[200px] lg:h-[245px] lg:w-[387px] mx-auto ' />
-                <div className='text-[#F1F1F1] text-[24px] leading-[26.4px] lg:text-[30px] font-bold lg:leading-[33px] text-center mx-auto mt-[43px] mb-[23px] w-[253px] lg:w-[316px] '>Real-Time Investment Insights</div>
-                <div className='text-[#fff] text-[16px] leading-[25.1px] lg:text-[22px] lg:leading-[34.62px] text-center mx-auto w-[336px] lg:w-[388px]  '>Stay informed with live investment data. Mchango_ provides real-time insights, alerts, and analysis for informed decision-making in the cryptocurrency market.</div>
-                <div className='text-[20px] font-bold text-[#418FAA] mx-auto text-center lg:mt-[45px] mt-[20px] cursor-pointer underline underline-offset-4 hover:text-[#fff] '>Get Started</div>
+      <div className="flex justify-center mt-[70px] lg:mt-[76px] mb-[50px] ">
+        <div className="w-full">
+          <p className=" text-[16px] sm:text-[24px] leading-[22px] font-semibold font-lexend  text-[#D9D9DB] lg:w-[734px] mx-auto text-center tracking-wide lg:leading-normal ">
+            Smart Crypto Investing <br className="sm:hidden" />
+            and Community Empowerment
+          </p>
+          <div className=" flex justify-center items-center">
+            <div className="grid lg:grid-cols-3 gap-5 justify-between">
+              {/**Investment Card */}
+              <div className="py-[32px] px-[10px] flex items-center justify-center">
+                <BackgroundGradient className=" py-3  ">
+                  <Image
+                    src={invest}
+                    alt="icon"
+                    className="w-[250px] h-[150px] lg:h-[245px] lg:w-[387px] mx-auto "
+                  />
+                  <p className="text-[#F1F1F1] text-[20px] leading-[26.4px] lg:text-[30px] font-lexend font-extrabold lg:leading-[33px] text-center mx-auto mt-[43px] mb-[23px] w-[253px] lg:w-[316px] ">
+                    Real-Time Investment Insights
+                  </p>
+
+                  <p className="text-[#fff] font-work font-medium text-[16px] leading-[25.1px] lg:text-[22px] lg:leading-[34.62px] text-center mx-auto w-[336px] lg:w-[388px] -pt-1 ">
+                    Stay informed with live investment data. Mchango_ provides
+                    real-time insights, alerts, and analysis for informed
+                    decision-making in the cryptocurrency market.
+                  </p>
+                  <button className="text-[16x] text-white font-lexend font-extrabold mx-auto text-center lg:mt-[45px] mt-[20px] cursor-pointer underline underline-offset-4 hover:scale-100 active:scale-90 transition-all duration-200 flex self-center ">
+                    Get Started
+                  </button>
+                </BackgroundGradient>
               </div>
-              <div className='smartbg py-[32px] px-[10px]'>
-                <Image src={collab} alt='icon' className='lg:w-[359px] w-[248px] h-[200px] lg:h-[254px] mx-auto ' />
-                <div className='text-[#F1F1F1] text-[24px] leading-[26.4px] lg:text-[30px] font-bold lg:leading-[33px] text-center mx-auto mt-[32.6px] mb-[23px] '>Community Collaboration for Profitability</div>
-                <div className='text-[#fff] text-[16px] leading-[25.1px] lg:text-[22px] lg:leading-[34.62px] text-center mx-auto w-[313px] lg:w-[388px] '>Boost profits through community collaboration. Join forces, share wisdom, manage risks, and achieve greater profitability in the volatile market.</div>
-                <div className='text-[20px] font-bold text-[#418FAA] mx-auto text-center mt-[20px] lg:mt-[76px] cursor-pointer underline underline-offset-4 hover:text-[#fff] '>Get Started</div>
+
+              {/**Collaboration Card */}
+              <div className=" py-[32px] px-[10px]">
+                <BackgroundGradient className=" py-3  ">
+                  <Image
+                    src={collab}
+                    alt="icon"
+                    className="w-[250px] h-[150px] lg:h-[245px] lg:w-[387px] mx-auto "
+                  />
+                  <p className="text-[#F1F1F1] text-[20px] leading-[26.4px] lg:text-[30px] font-lexend font-extrabold lg:leading-[33px] text-center mx-auto mt-[43px] mb-[23px] w-[253px] lg:w-[316px] ">
+                    Community Collaboration for Profitability
+                  </p>
+                  <p className="text-[#fff] font-work font-medium text-[16px] leading-[25.1px] lg:text-[22px] lg:leading-[34.62px] text-center mx-auto w-[336px] lg:w-[388px]  ">
+                    Boost profits through community collaboration. Join forces,
+                    share wisdom, manage risks, and achieve greater
+                    profitability in the volatile market.
+                  </p>
+                  <button className="text-[16x] text-white font-lexend font-extrabold mx-auto text-center lg:mt-[45px] mt-[20px] cursor-pointer underline underline-offset-4 hover:scale-100 active:scale-90 transition-all duration-200 flex self-center ">
+                    Get Started
+                  </button>
+                </BackgroundGradient>
               </div>
-              <div className='smartbg py-[32px] px-[10px]'>
-                <Image src={auto} alt='icon' className='lg:w-[363px] w-[262.8px] h-[200px] lg:h-[257px]  mx-auto ' />
-                <div className='text-[#F1F1F1]  text-[24px] leading-[26.4px] lg:text-[30px] font-bold lg:leading-[33px] lg:w-[373px] mx-auto mt-[15px] text-center lg:mt-[28px] mb-[20px] lg:mb-[55px]' >Automated Group Savings</div>
-                <div className='text-[#fff] lg:text-[22px] text-[16px] leading-[25.1px] lg:leading-[34.62px] text-center mx-auto  w-[327px] lg:w-[388px]'>Join or create savings groups effortlessly. Mchango_ automates contributions, ensuring disciplined group savings for common financial goals.</div>
-                <div className='text-[20px] font-bold text-[#418FAA] mx-auto text-center mt-[20px] lg:mt-[76px] cursor-pointer underline underline-offset-4 hover:text-[#fff] '>Get Started</div>
+
+              {/**Automated Card */}
+              <div className=" py-[32px] px-[10px]">
+                <BackgroundGradient className="py-3">
+                  <Image
+                    src={auto}
+                    alt="icon"
+                    className="w-[250px] h-[170px] lg:h-[245px] lg:w-[387px] mx-auto "
+                  />
+                  <p className="text-[#F1F1F1] text-[20px] leading-[26.4px] lg:text-[30px] font-lexend font-extrabold lg:leading-[33px] text-center mx-auto mt-[43px] mb-[23px] w-[253px] lg:w-[316px] ">
+                    Automated Group Savings
+                  </p>
+                  <p className="text-[#fff] font-work font-medium text-[16px] leading-[25.1px] lg:text-[22px] lg:leading-[34.62px] text-center mx-auto w-[336px] lg:w-[388px] pt-9 ">
+                    Join or create savings groups effortlessly. Mchango_
+                    automates contributions, ensuring disciplined group savings
+                    for shared financial objectives.
+                  </p>
+                  <button className="text-[16x] text-white font-lexend font-extrabold mx-auto text-center lg:mt-[45px] mt-[20px] cursor-pointer underline underline-offset-4 hover:scale-100 active:scale-90 transition-all duration-200 flex self-center ">
+                    Get Started
+                  </button>
+                </BackgroundGradient>
               </div>
             </div>
-
           </div>
         </div>
-
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SmartCrypto
+export default SmartCrypto;
