527e8b7336ed9ac809ff7ada1f21de9b0601b8de
3illBaby <thrillanon@outlook.com>
2024-02-09 00:20:42 +0100

    components: text animation

---
+++ frontend/app/components/CustomTexts.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { staggerContainer, textContainer, textVariant2 } from '@/utils/motion';
+
+type TypingTextProps = {
+  title: string;
+  textStyles: React.CSSProperties | string;
+};
+
+export const TypingText = ({ title, textStyles }: TypingTextProps) => (
+  <motion.div
+    variants={staggerContainer(0.1, 0.1)}
+    initial="hidden"
+    whileInView="show"
+    viewport={{ once: false, amount: 0.25 }}
+  >
+    <motion.p variants={textContainer} className={`${textStyles}`}>
+      {Array.from(title).map((letter, index) => (
+        <motion.span variants={textVariant2} key={index}>
+          {letter === ' ' ? '\u00a0' : letter}
+        </motion.span>
+      ))}
+    </motion.p>
+  </motion.div>
+);
+
+export const TitleText = ({ title, textStyles }: TypingTextProps) => (
+  <motion.div
+    variants={staggerContainer(0.1, 0.1)}
+    initial="hidden"
+    whileInView="show"
+    viewport={{ once: false, amount: 0.25 }}
+  >
+    <motion.h2
+      variants={textVariant2}
+      initial="hidden"
+      whileInView="show"
+      className={` ${textStyles}`}
+    >
+      {title}
+    </motion.h2>
+  </motion.div>
+);
