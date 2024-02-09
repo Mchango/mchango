74e7f23002fd27e2946586819fae572283902aa8
3illBaby <thrillanon@outlook.com>
2024-02-09 19:09:54 +0100

    minor tweak

--- frontend/app/components/Plan.tsx
+++ frontend/app/components/Plan.tsx
@@ -76,7 +76,7 @@ const Plan = () => {
 
         {/**Premium plan */}
         <PinContainer
-          title="Basic Plan"
+          title="Best Offer"
           href="/"
           className="w-[300px] h-[500px] bg-coolGray-950 bg-opacity-70 backdrop-blur-md rounded-[20px] p-[30px] flex flex-col gap-5 text-white "
         >
@@ -126,111 +126,6 @@ const Plan = () => {
             </button>
           </div>
         </PinContainer>
-
-        {/* <div className="grid grid-cols-3 justify-between space-x-[10px]">
-          <div className="smartbg px-[40px] pt-[40px] ">
-            <Image src={basic} alt="icon" className="mb-[20px]" />
-            <div className="text-[#00FFFF] text-[44px] font-medium tracking-[-1px] leading-normal ">
-              Basic Plan
-            </div>
-            <div className="text-[24px] leading-[32px] text-[#fff] w-[95%] mt-[14px] ">
-              The Basic Plan is ideal for users new to collective savings. Start
-              your journey towards financial goals with group savings power.
-            </div>
-            <hr className="w-[90%] my-[28px] bg-[#E7EBFF] " />
-            <div className="flex">
-              <Image src={Check} alt="checks" className="mr-[16px]" />
-              <div className="text-[24px] leading-[40px] text-[#0ff] ">
-                Commission Fee: 3%
-              </div>
-            </div>
-            <div className="flex mt-[12px] ">
-              <Image src={Check} alt="checks" className="mr-[16px]" />
-              <div className="text-[24px] leading-[40px] text-[#0ff] w-[80%] ">
-                No of subscriibers: 10 max
-              </div>
-            </div>
-            <div className="flex mt-[12px] ">
-              <Image src={Check} alt="checks" className="mr-[16px]" />
-              <div className="text-[24px] leading-[40px] text-[#0ff] w-[80%] ">
-                Subscription Payment: 0 ETH
-              </div>
-            </div>
-            <button className="text-[#00FFFF] rounded-[12px] pt-[18px] border-[1px] border-[#6D6D6D] mt-[44px] mx-auto flex justify-center mb-[40px] w-[320px] h-[60px] ">
-              Get Started
-            </button>
-          </div>
-          <div className="smartbg px-[40px] pt-[40px] ">
-            <div className="flex mb-[20px]">
-              <Image src={premium} alt="premium" className="" />
-              <button className="text-[#FFFFFF] border-[1px] border-[#fff] bg-[rgba(255, 255, 255, 0.08)] rounded-[8px] py-[5px] px-[10px] text-[18px] font-light w-[103px] h-[38px] my-auto ml-[20px] leading-normal ">
-                Best offer
-              </button>
-            </div>
-            <div className="text-[#00FFFF] text-[44px] font-medium tracking-[-1px] leading-normal ">
-              Premium Plan
-            </div>
-            <div className="text-[24px] leading-[32px] text-[#fff] w-[95%] mt-[14px] ">
-              Elevate savings with the Premium Plan's advanced features and
-              exclusive benefits, designed for accelerated financial success.{' '}
-            </div>
-            <hr className="w-[90%] my-[28px] bg-[#E7EBFF] " />
-            <div className="flex">
-              <Image src={Checkdark} alt="icon" className="mr-[16px]" />
-              <div className="text-[24px] leading-[40px] text-[#1B223C] ">
-                Commission Fee: 1.5%
-              </div>
-            </div>
-            <div className="flex mt-[12px] ">
-              <Image src={Checkdark} alt="icon" className="mr-[16px]" />
-              <div className="text-[24px] leading-[40px] text-[#1B223C] w-[80%] ">
-                No of subscriibers: 30 max
-              </div>
-            </div>
-            <div className="flex mt-[12px] ">
-              <Image src={Checkdark} alt="icon" className="mr-[16px]" />
-              <div className="text-[24px] leading-[40px] text-[#1B223C] w-[80%] ">
-                Subscription Payment: 0.05 ETH
-              </div>
-            </div>
-            <button className="text-[#00FFFF] rounded-[12px] pt-[18px] border-[1px] border-[#6D6D6D] mt-[44px] mx-auto flex justify-center mb-[40px] w-[320px] h-[60px] ">
-              Get Started
-            </button>
-          </div>
-          <div className="smartbg px-[40px] pt-[40px] ">
-            <Image src={basic} alt="icon" className="mb-[20px]" />
-            <div className="text-[#00FFFF] text-[44px] font-medium tracking-[-1px] leading-normal ">
-              Exclusive Plan
-            </div>
-            <div className="text-[24px] leading-[32px] text-[#fff] w-[95%] mt-[14px] ">
-              Start with the Basic Plan for an entry into collective savings.
-              Begin your journey to financial goals and benefit from group
-              savings power.
-            </div>
-            <hr className="w-[90%] my-[28px] bg-[#E7EBFF] " />
-            <div className="flex">
-              <Image src={Check} alt="checks" className="mr-[16px]" />
-              <div className="text-[24px] leading-[40px] text-[#0ff] ">
-                Commission Fee: 1%
-              </div>
-            </div>
-            <div className="flex mt-[12px] ">
-              <Image src={Check} alt="checks" className="mr-[16px]" />
-              <div className="text-[24px] leading-[40px] text-[#0ff] w-[80%] ">
-                No of subscriibers: 50 max
-              </div>
-            </div>
-            <div className="flex mt-[12px] ">
-              <Image src={Check} alt="checks" className="mr-[16px]" />
-              <div className="text-[24px] leading-[40px] text-[#0ff] w-[80%] ">
-                Subscription Payment: 0.1 ETH
-              </div>
-            </div>
-            <button className="text-[#00FFFF] rounded-[12px] pt-[18px] border-[1px] border-[#6D6D6D] mt-[44px] mx-auto flex justify-center mb-[40px] w-[320px] h-[60px] ">
-              Get Started
-            </button>
-          </div>
-        </div> */}
       </div>
     </div>
   );
