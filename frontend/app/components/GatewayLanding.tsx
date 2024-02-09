b874d6edacb3843aa5c5ced5ae29a014ba118b5f
3illBaby <thrillanon@outlook.com>
2024-02-09 11:26:59 +0100

    optimization

--- frontend/app/components/GatewayLanding.tsx
+++ frontend/app/components/GatewayLanding.tsx
@@ -1,13 +1,8 @@
 import Image from 'next/image';
 import React from 'react';
 import mchlogo from '../assets/mchlogo.svg';
-import { TypewriterEffectSmooth } from './TypewritterEffect';
+import { TitleText, TypingText } from './CustomTexts';
 
-const wordsToAnimate = [{ text: 'Begin contributing, saving, and growing.' }];
-
-const typeWritterClass =
-  'text-[20px] sm:text-[20px] lg:text-[30px] font-satoshi leading-[19.1px] font-medium sm:leading-[32px] tracking-[0.72px] sm:tracking-wider text-center builbtn-grad lg:mt-[30px] mt-[21px] mb-[52px]';
-const cursorClass = 'bg-gray-400';
 const GatewayLanding = () => {
   return (
     <div className="flex justify-center mt-[53px] lg:mt-[88px] z-50">
@@ -18,10 +13,11 @@ const GatewayLanding = () => {
             alt="mchlogo"
             className="sm:w-[110px] sm:h-[100px] w-[60px] h-[50px] "
           /> */}
-
-          <h1 className="text-[40px] sm:text-[50px] lg:text-[70px] font-lexend text-[#FFF] font-extrabold leading-[43px] lg:leading-normal my-auto ml-[15px] ">
-            Mchango_
-          </h1>
+          <TitleText
+            textStyles={`text-[40px] sm:text-[50px] lg:text-[70px] font-lexend text-[#FFF] font-extrabold leading-[43px] lg:leading-normal my-auto ml-[15px] `}
+          >
+            <p>Mchango_</p>
+          </TitleText>
         </div>
         <h2 className=" text-[28px] sm:text-[50px] font-lexend font-bold leading-[32px] sm:leading-[50px]  text-gra tracking-wider text-center mb-[21px] z-20">
           Gateway to Smart Savings and Contributions
@@ -36,10 +32,9 @@ const GatewayLanding = () => {
         </h3>
 
         <div className="flex justify-center">
-          <TypewriterEffectSmooth
-            words={wordsToAnimate}
-            className={typeWritterClass}
-            cursorClassName={cursorClass}
+          <TypingText
+            title="Begin contributing, saving, and growing."
+            textStyles="text-[18px] sm:text-[20px] lg:text-[30px] font-satoshi leading-[19.1px] font-medium sm:leading-[32px] tracking-[0.72px] sm:tracking-wider text-center builbtn-grad lg:mt-[30px] mt-[21px] mb-[52px]"
           />
         </div>
 
