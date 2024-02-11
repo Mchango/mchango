7ab46c739b2f0f1dd15a90f8acb840ceaacbf31b
3illBaby <thrillanon@outlook.com>
2024-02-12 00:07:33 +0100

    refactor(root): imported mobile navigation

--- frontend/app/(root)/page.tsx
+++ frontend/app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import GatewayLanding from '../components/GatewayLanding';
 import MasteryHub from '../components/MasteryHub';
+import MobileNav from '../components/MobileNav';
 import Navbar from '../components/Navbar';
 import Plan from '../components/Plan';
 import SmartCrypto from '../components/SmartCrypto';
@@ -17,6 +18,9 @@ const Landing = () => {
           <div className='h-full w-full bg-[url("https://res.cloudinary.com/dmye53wps/image/upload/v1699652923/mchango_m0fuc7.svg")] bg-cover bg-center relative  '>
             <div className=" absolute bg-grid-white/[0.1] h-full w-full"></div>
             <div className="lg:max-w-7xl w-full mx-auto z-50 relative">
+              <div className="absolute top-[80px] left-[170px] z-50 ">
+                <MobileNav />
+              </div>
               <Navbar />
               <GatewayLanding />
               <SmartCrypto />
