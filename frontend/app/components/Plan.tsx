cdeb652a2e485eb7eb807d56ca3114c2ec06b591
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-02-14 16:47:01 +0100

    feat: plan responsiveness

--- frontend/app/components/Plan.tsx
+++ frontend/app/components/Plan.tsx
@@ -10,10 +10,10 @@ const Plan = () => {
         <>
             <div className='flex justify-center mb-[150px]'>
                 <div className='w-full'>
-                    <div className='text-[#F1F1F1] text-[50px] font-extrabold mx-auto text-center leading-normal '>Find Your Perfect Plan</div>
-                    <div className='text-[24px] font-light leading-[34px] text-[#D0D0D0] w-[70%] mx-auto text-center mt-[14px] mb-[60px] '>Our transparent and accessible pricing structures are designed to cater to diverse needs. Choose a plan that suits you, whether you're an individual trader or part of a dynamic investment team</div>
+                    <h3 className={`text-[#F1F1F1] text-[24px] sm:text-[50px] font-lexend font-extrabold mx-auto text-center leading-normal` }>Find Your Perfect Plan</h3>
+                    <p className='text-[14px] sm:text-[24px] font-light leading-[34px] text-[#D0D0D0] w-[85%] sm:w-[70%] mx-auto text-center mt-[14px] mb-[60px] '>Our transparent and accessible pricing structures are designed to cater to diverse needs. Choose a plan that suits you, whether you're an individual trader or part of a dynamic investment team</p>
                     <div>
-                        <div className='grid grid-cols-3 justify-between space-x-[10px]' >
+                        <div className='grid lg:grid-cols-3 justify-between space-x-[10px]' >
                             <div className='smartbg px-[40px] pt-[40px] '>
                                 <Image src={basic} alt='icon' className='mb-[20px]' />
                                 <div className='text-[#00FFFF] text-[44px] font-medium tracking-[-1px] leading-normal '>Basic Plan</div>
@@ -63,7 +63,7 @@ const Plan = () => {
                             <div className='smartbg px-[40px] pt-[40px] '>
                                 <Image src={basic} alt='icon' className='mb-[20px]' />
                                 <div className='text-[#00FFFF] text-[44px] font-medium tracking-[-1px] leading-normal '>Exclusive Plan</div>
-                                <div className='text-[24px] leading-[32px] text-[#fff] w-[95%] mt-[14px] '>Start with the Basic Plan for an entry into collective savings. Begin your journey to financial goals and benefit from group savings power.</div>
+                                <p className='text-[24px] leading-[32px] text-[#fff] w-[95%] mt-[14px] '>Start with the Basic Plan for an entry into collective savings. Begin your journey to financial goals and benefit from group savings power.</p>
                                 <hr className='w-[90%] my-[28px] bg-[#E7EBFF] ' />
                                 <div className='flex'>
                                     <Image src={Check} alt='checks' className='mr-[16px]' />
