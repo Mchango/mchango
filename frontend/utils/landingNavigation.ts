add7ff7e9746a7381a586c3978e157504d426170
3illBaby <thrillanon@outlook.com>
2024-02-25 21:04:35 +0100

    chore(utils): implemented function to set cookie on connect wallet and handle existing user

--- frontend/utils/landingNavigation.ts
+++ frontend/utils/landingNavigation.ts
@@ -1,27 +1,50 @@
 import { useRouter } from 'next/navigation'
 import { useWallet } from '@/context/connectWallet'
+import { connectAndValidate } from '@/actions'
+import Cookies from 'js-cookie'
 
 const useAppNavigation = () => {
   const router = useRouter()
-  const { connectWallet } = useWallet()
+  const { connectWallet, account } = useWallet()
 
   const handleNavLinks = {
     handleGetStartedToggled: () => {
       router.push('/signup')
     },
 
-    handleSignInToggled: (routeName: string) => {
+    handleSignInToggled: async (routeName: string) => {
       if (routeName === '/signin') {
         try {
           connectWallet()
+          if (account) {
+            Cookies.set('walletConnected', 'true', {
+              expires: 2, // 2 days
+            })
+          }
         } catch (error) {
-          throw new MetaMaskError()
+          throw new MetaMaskNotInstalledError()
         }
       } else if (routeName === '#pricing') {
         const pricingSection = document.getElementById('pricing')
         if (pricingSection) {
           pricingSection.scrollIntoView({ behavior: 'smooth' })
         }
+      }
+    },
+
+    handleExistingUser: async () => {
+      try {
+        if (account) {
+          const isMember = await connectAndValidate(account as string)
+          console.log(isMember)
+
+          if (isMember === true) {
+            router.push('/home')
+          }
+        }
+      } catch (error) {
+        console.error(error)
+        throw new Error('An error occurred while validating member')
       }
     },
   }
