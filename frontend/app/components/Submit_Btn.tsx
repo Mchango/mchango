0d89f31e2f61d1bffc6fb823e470f1ece28069a0
3illBaby <thrillanon@outlook.com>
2024-03-01 16:49:48 +0100

    refactor(Submit_Btn): refactored component

--- frontend/app/components/Submit_Btn.tsx
+++ frontend/app/components/Submit_Btn.tsx
@@ -1,18 +1,42 @@
+"use client"
+
 import React from 'react';
 import { useFormStatus } from 'react-dom';
-import { Lock } from 'lucide-react';
-import { Send } from 'lucide-react';
 
-const Submit_Btn = () => {
+type ButtonProps = {
+  backgroundColor: string;
+  name: string;
+  padding?: string;
+  fontColor?: string;
+  fontSize?: string;
+};
+
+const Submit_Btn = ({
+  backgroundColor,
+  name,
+  padding,
+  fontColor,
+  fontSize,
+}: ButtonProps) => {
   const { pending } = useFormStatus();
   return (
-    <button className="bg-white border-2  group hover:bg-[#008080]  hover:scale-110 active:scale-105 transition-all duration-200 rounded-[15px] shadow-md p-2 items-center  w-[fit-content]">
+    <button
+      className={`${backgroundColor} border-2  group hover:bg-[#008080]  hover:scale-110 active:scale-105 transition-all duration-200 rounded-[15px] shadow-md ${
+        padding ? padding : 'p-2'
+      } items-center  w-[fit-content]`}
+    >
       {pending ? (
         <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-[#008080]"></div>
       ) : (
         <div className="flex items-center gap-[2]">
-          <span className="font-Azeret font-semibold text-[12px] text-gray-950 group-hover:text-white sm:text-[16px]">
-            Subscribe
+          <span
+            className={`font-Azeret font-semibold ${
+              fontSize ? fontSize : 'text-[12px] sm:text-[16px]'
+            } ${
+              fontColor ? fontColor : 'text-gray-950'
+            } group-hover:text-white `}
+          >
+            {name}
           </span>
         </div>
       )}
