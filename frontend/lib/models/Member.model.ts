28ca84ef55b567b9e074b477625926a1d95b6acb
3illBaby <thrillanon@outlook.com>
2024-03-01 16:52:42 +0100

    refactor(Member): refactored member schema

--- frontend/lib/models/Member.model.ts
+++ frontend/lib/models/Member.model.ts
@@ -3,6 +3,10 @@ import mongoose from 'mongoose'
 const MemberSchema = new mongoose.Schema({
   id: { type: Number, required: true, unique: true },
   name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  country: { type: String, required: true },
+  phone: { type: String, required: true, unique: true },
   memberAddress: { type: String, required: true, unique: true },
   amountDonated: { type: Number },
   amountCollected: { type: Number },
