e1e78123d2a8e8850f422e83c2a3bf2a47a2b208
Oloruntobi <oloruntobialademehin@gmail.com>
2024-03-04 15:49:01 +0100

    adjusted the join button one

--- frontend/app/components/JoinGroupBtn.tsx
+++ frontend/app/components/JoinGroupBtn.tsx
@@ -3,7 +3,7 @@ import MainArrorwBlackSvg from "../svgComponents/mainArrorwBlackSvg";
 
 export default function JoinGroupBtn() {
   return (
-    <div className="w-[304px] p-[28px] items-center justify-center gap-[12px] bg-deepGreen flex my-0 mx-auto  rounded-2xl">
+    <div className="w-[304px] p-[28px] items-center justify-center gap-[12px] bg-deepGreen flex my-0 mx-auto  rounded-2xl cursor-pointer">
       <p className="text-black font-poppins font-normal not-italic text-2xl">
         Join Group
       </p>
