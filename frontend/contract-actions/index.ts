988be4a78e81a013c42d498bfffe9c1f69a86f4d
Oloruntobi <oloruntobialademehin@gmail.com>
2024-10-14 23:31:03 +0100

    "Formatted code in frontend/contract-actions/index.ts: added/removed whitespace, changed quotes from single to double and vice versa, and reformatted some lines."

--- frontend/contract-actions/index.ts
+++ frontend/contract-actions/index.ts
@@ -1,92 +1,93 @@
-import { ethers } from 'ethers'
-import { abi } from './abi'
-import type { joinCreatedGroupType } from '@/lib/types'
+import { ethers } from "ethers";
+import { abi } from "./abi";
+import type { joinCreatedGroupType } from "@/lib/types";
 
 const contractAddress =
-  process.env.CONTRACT_ADDRESS || '0x6216E99E1d39A9723f2756E839051c65d05736d4'
+  process.env.CONTRACT_ADDRESS || "0x6216E99E1d39A9723f2756E839051c65d05736d4";
 
 const getProviderAndSigner = async () => {
-  if (typeof window === 'undefined' || !(window as any).ethereum) {
-    throw new MetaMaskNotInstalledError()
+  if (typeof window === "undefined" || !(window as any).ethereum) {
+    throw new MetaMaskNotInstalledError();
   }
 
-  const provider = new ethers.providers.Web3Provider((window as any).ethereum)
-  const signer = provider.getSigner()
-  return { provider, signer }
-}
+  const provider = new ethers.providers.Web3Provider((window as any).ethereum);
+  const signer = provider.getSigner();
+  return { provider, signer };
+};
 
 const createNewMemberWithSigner = async (
   signer: ethers.Signer,
   contractAddress: string,
-  abi: any,
+  abi: any
 ) => {
-  const signerAddress = await signer.getAddress()
-  const contract = new ethers.Contract(contractAddress, abi, signer)
+  const signerAddress = await signer.getAddress();
+  const contract = new ethers.Contract(contractAddress, abi, signer);
 
-  const transaction = await contract.createNewMember(signerAddress)
-  await transaction.wait()
+  const transaction = await contract.createNewMember(signerAddress);
+  await transaction.wait();
 
-  const id = await contract.memberCounter()
-  const formatted = id.toNumber()
+  const id = await contract.memberCounter();
+  const formatted = id.toNumber();
 
-  return [formatted, signerAddress]
-}
+  return [formatted, signerAddress];
+};
 
 const subscribePremiumWithSigner = async (
   signer: ethers.Signer,
   contractAddress: string,
   abi: any,
-  premiumValue: ethers.BigNumber | number,
+  premiumValue: ethers.BigNumber | number
 ) => {
-  const signerAddress = await signer.getAddress()
-  const contract = new ethers.Contract(contractAddress, abi, signer)
+  const signerAddress = await signer.getAddress();
+  const contract = new ethers.Contract(contractAddress, abi, signer);
 
   const transaction = await contract.subscribePremium({
     value: premiumValue,
-  })
-  await transaction.wait()
+  });
+  await transaction.wait();
 
-  return [signerAddress, premiumValue]
-}
+  return [signerAddress, premiumValue];
+};
 
 const createNewGroupWithSigner = async (
   signer: ethers.Signer,
   contractAddress: string,
   abi: any,
-  collateralValue: string,
+  collateralValue: string
 ) => {
-  const signerAddress = await signer.getAddress()
-  const contract = new ethers.Contract(contractAddress, abi, signer)
+  const signerAddress = await signer.getAddress();
+  const contract = new ethers.Contract(contractAddress, abi, signer);
 
-  const result = valueFormatter(collateralValue)
-  if (!result) throw new Error('Error parsing value')
+  const result = valueFormatter(collateralValue);
+  if (!result) throw new Error("Error parsing value");
 
-  const [formattedValue, numberFormat] = result
+  const [formattedValue, numberFormat] = result;
 
-  const txResponse = await contract.createGroup(formattedValue)
-  await txResponse.wait()
+  const txResponse = await contract.createGroup(formattedValue);
+  await txResponse.wait();
 
-  const groupId = await contract.counter()
-  const formatted = groupId.toNumber()
+  const groupId = await contract.counter();
+  const formatted = groupId.toNumber();
 
-  return [formatted, numberFormat as number, signerAddress as string]
-}
+  return [formatted, numberFormat as number, signerAddress as string];
+};
 
 const joinGroupWithSigner = async (
   signer: ethers.Signer,
-  { id, amount, collateralValue, reputationPoint }: joinCreatedGroupType,
+  { id, amount, collateralValue, reputationPoint }: joinCreatedGroupType
 ) => {
   try {
-    const signerAddress = await signer.getAddress()
-    const contract = new ethers.Contract(contractAddress, abi, signer)
+    const signerAddress = await signer.getAddress();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
 
-    const result = valueFormatter(amount)
-    if (!result) throw new Error('Error parsing value')
+    const result = valueFormatter(amount);
+    if (!result) throw new Error("Error parsing value");
 
-    const [formattedValue, numberFormat] = result
-    if (!formattedValue || !numberFormat) throw new Error('Error parsing value')
+    const [formattedValue, numberFormat] = result;
+    if (!formattedValue || !numberFormat)
+      throw new Error("Error parsing value");
 
-    const formattedCollateralValue = BigInt(collateralValue)
+    const formattedCollateralValue = BigInt(collateralValue);
 
     const txResponse = await contract.joinGroup(
       signerAddress,
@@ -95,86 +96,84 @@ const joinGroupWithSigner = async (
       reputationPoint,
       {
         value: formattedValue,
-      },
-    )
+      }
+    );
 
-    await txResponse.wait()
-    return `User ${signerAddress} joined group ${id} successfully`
+    await txResponse.wait();
+    return `User ${signerAddress} joined group ${id} successfully`;
   } catch (error) {
-    console.error(`An error occurred: ${error}`)
-    throw new JoinGroupError()
+    console.error(`An error occurred: ${error}`);
+    throw new JoinGroupError();
   }
-}
+};
 
 const startContributionWithSigner = async (
   signer: ethers.Signer,
   contractAddress: string,
   abi: any,
   id: number,
-  contributionValue: number,
+  contributionValue: number
 ) => {
   try {
-    const contract = new ethers.Contract(contractAddress, abi, signer)
-    const txResponse = await contract.startContribution(id, contributionValue)
-    await txResponse.wait()
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    const txResponse = await contract.startContribution(id, contributionValue);
+    await txResponse.wait();
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 const valueFormatter = (value: string) => {
   try {
-    const formattedValue = ethers.utils.parseUnits(value, 'ether')
-    const numberFormat = Number(formattedValue.toString())
-    return [formattedValue, numberFormat]
+    const formattedValue = ethers.utils.parseUnits(value, "ether");
+    const numberFormat = Number(formattedValue.toString());
+    return [formattedValue, numberFormat];
   } catch (error) {
-    console.error(`An error occurred: ${error}`)
+    console.error(`An error occurred: ${error}`);
   }
-}
+};
 
+//creating new member
 const createNewMember = async (): Promise<[number, string]> => {
   try {
-    const { signer } = await getProviderAndSigner()
+    const { signer } = await getProviderAndSigner();
     const [formatted, signerAddress] = await createNewMemberWithSigner(
       signer,
       contractAddress,
-      abi,
-    )
-    return [formatted, signerAddress]
+      abi
+    );
+    return [formatted, signerAddress];
   } catch (error) {
     if (error instanceof MetaMaskNotInstalledError) {
-      throw error
+      throw error;
     }
-    console.error(`An error occurred: ${error}`)
-    throw new Error('An error occurred while creating a new member')
+    console.error(`An error occurred: ${error}`);
+    throw new Error("An error occurred while creating a new member");
   }
-}
+};
 
 const createNewGroup = async (
-  collateralValue: string,
+  collateralValue: string
 ): Promise<[number, number, string]> => {
   try {
-    const { signer } = await getProviderAndSigner()
-    const [
-      formatted,
-      numberFormat,
-      signerAddress,
-    ] = await createNewGroupWithSigner(
-      signer,
-      contractAddress,
-      abi,
-      collateralValue,
-    )
+    const { signer } = await getProviderAndSigner();
+    const [formatted, numberFormat, signerAddress] =
+      await createNewGroupWithSigner(
+        signer,
+        contractAddress,
+        abi,
+        collateralValue
+      );
 
-    return [formatted, numberFormat, signerAddress]
+    return [formatted, numberFormat, signerAddress];
   } catch (error) {
     if (error instanceof MetaMaskNotInstalledError) {
-      throw error
+      throw error;
     }
-    console.error(`An error occurred: ${error}`)
-    throw new Error('An error occurred while creating a new group')
+    console.error(`An error occurred: ${error}`);
+    throw new Error("An error occurred while creating a new group");
   }
-}
+};
 
 const joinCreatedGroup = async ({
   id,
@@ -183,62 +182,62 @@ const joinCreatedGroup = async ({
   reputationPoint,
 }: joinCreatedGroupType) => {
   try {
-    const { signer } = await getProviderAndSigner()
+    const { signer } = await getProviderAndSigner();
     const result = await joinGroupWithSigner(signer, {
       id,
       amount,
       collateralValue,
       reputationPoint,
-    })
-    return result
+    });
+    return result;
   } catch (error) {
     if (error instanceof MetaMaskNotInstalledError) {
-      throw error
+      throw error;
     }
-    console.error(`An error occurred: ${error}`)
-    throw new Error('An error occurred while joining a group')
+    console.error(`An error occurred: ${error}`);
+    throw new Error("An error occurred while joining a group");
   }
-}
+};
 
 const subscribePremiumUser = async (): Promise<[string, number]> => {
   try {
-    const result = valueFormatter('0.002')
+    const result = valueFormatter("0.002");
 
-    if (!result) throw new Error('Error parsing value')
+    if (!result) throw new Error("Error parsing value");
 
-    const [formattedValue, numberFormat] = result
-    const { signer } = await getProviderAndSigner()
+    const [formattedValue, numberFormat] = result;
+    const { signer } = await getProviderAndSigner();
 
     const [signerAddress, premiumValue] = await subscribePremiumWithSigner(
       signer,
       contractAddress,
       abi,
-      formattedValue,
-    )
+      formattedValue
+    );
 
-    if (!signerAddress || !premiumValue) throw new Error('Error parsing value')
-    return [signerAddress as string, numberFormat as number]
+    if (!signerAddress || !premiumValue) throw new Error("Error parsing value");
+    return [signerAddress as string, numberFormat as number];
   } catch (error) {
-    console.error(error)
-    throw new SubscriptionError()
+    console.error(error);
+    throw new SubscriptionError();
   }
-}
+};
 
 const StartContribution = async (id: number, contributionValue: number) => {
   try {
-    const { signer } = await getProviderAndSigner()
+    const { signer } = await getProviderAndSigner();
     await startContributionWithSigner(
       signer,
       contractAddress,
       abi,
       id,
-      contributionValue,
-    )
+      contributionValue
+    );
   } catch (error) {
-    console.error(error)
-    throw new StartContributionError()
+    console.error(error);
+    throw new StartContributionError();
   }
-}
+};
 
 export {
   createNewMember,
@@ -248,4 +247,4 @@ export {
   valueFormatter,
   subscribePremiumUser,
   StartContribution,
-}
+};
